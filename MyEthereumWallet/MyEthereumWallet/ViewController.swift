eb4212d3acc70c8e45b93d4c0eb5a966fd8ca0b2
Rob Enriquez <rob@goabra.com>
2017-05-30 22:14:57 -0700

    updated pod

--- MyEthereumWallet/MyEthereumWallet/ViewController.swift
+++ MyEthereumWallet/MyEthereumWallet/ViewController.swift
@@ -7,63 +7,65 @@
 //
 
 import UIKit
-//import Geth
+import Geth
 
 class ViewController: UIViewController {
 
     //constants
-    let keyStoreKey = "/keystore2"
+    let keyStoreKey = "/keystore"
     let passwordKey = "Creation password"
     let importPasswordKey = "Import password"
     let exportPaswordKey = "Export password"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupAccount()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-//        setupAccount()
     }
 
-//    func setupAccount() {
-//        guard let dataDir: String = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first else {
-//            // TODO: add error logging
-//            NSLog("Failed to create dataDir")
-//            return
-//        }
-//
-//        guard let am = GEGethNewAccountManager(dataDir + keyStoreKey, GEGethStandardScryptN, GEGethStandardScryptP) else {
-//            // TODO: add error logging
-//            NSLog("Failed to instantitiate account manager")
-//            return
-//        }
-//        
-//        do {
-//            //create new account
-//            var newAcc: GEGethAccount?
-//            try am.newAccount(passwordKey, ret0_: &newAcc)
-//            print("NEW: \(String(describing: newAcc?.getAddress().getHex()))")
-//            
-//            //JSON key
-//            var jsonKey: NSData?
-//            try am.exportKey(newAcc, passphrase: passwordKey, newPassphrase: exportPaswordKey, ret0_: &jsonKey)
-//            let jsonString = String(data: jsonKey! as Data, encoding: .utf8)
-//            print("JSON: \(String(describing: jsonString))")
-//            
-//            //delete account
-//            try am.delete(newAcc, passphrase: passwordKey)
-//            let accsString = String(am.getAccounts().size())
-//            print("Accs: \(accsString)")
-//            
-//            var impAcc: GEGethAccount?
-//            try am.importKey(jsonKey! as Data, passphrase: exportPaswordKey, newPassphrase: importPasswordKey, ret0_: &impAcc)
-//            let impAccString = String(describing: impAcc?.getAddress().getHex())
-//            print("Imp: \(impAccString)")
-//            
-//        } catch {
-//            NSLog("Failed to run process")
-//        }
-//    }
+    func setupAccount() {
+        guard let dataDir: String = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first else {
+            // TODO: add error logging
+            NSLog("Failed to create dataDir")
+            return
+        }
+
+        guard let ks = GethNewKeyStore(dataDir + keyStoreKey, GethLightScryptN, GethLightScryptP) else {
+            // TODO: add error logging
+            NSLog("Failed to instantitiate account manager")
+            return
+            
+        }
+        
+        do {
+            // Create a new account with the specified encryption passphrase.
+            let newAcc = try ks.newAccount("Creation password")
+            print("NEW: \(String(describing: newAcc.getAddress().getHex()))")
+
+            // Export the newly created account with a different passphrase. The returned
+            // data from this method invocation is a JSON encoded, encrypted key-file.
+            let jsonKey = try ks.exportKey(newAcc, passphrase: "Creation password", newPassphrase: "Export password")
+            print("JSON: \(String(describing: jsonKey))")
+
+            // Update the passphrase on the account created above inside the local keystore.
+            try ks.update(newAcc, passphrase: "Creation password", newPassphrase: "Update password")
+            print("UPDATE: \(String(describing: newAcc.getAddress().getHex()))")
+            
+            // Delete the account updated above from the local keystore.
+            try ks.delete(newAcc, passphrase: "Update password")
+            print("DELETE: \(newAcc)")
+
+            // Import back the account we've exported (and then deleted) above with yet
+            // again a fresh passphrase.
+            let impAcc  = try ks.importKey(jsonKey, passphrase: "Export password", newPassphrase: "Import password")
+            print("IMPORT: \(impAcc)")
+
+        } catch {
+            NSLog("Failed to run process")
+        }
+    }
 }
 
